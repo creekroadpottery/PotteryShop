52d4c2fd1f65ee4112b73e627284b6fcf054d5a5
Alford Wayman <creekroadpottery@gmail.com>
2025-09-27 08:51:47 -0400

    Update PotteryShop.py

--- PotteryShop.py
+++ PotteryShop.py
@@ -1134,7 +1134,7 @@ elif menu == "Dashboard":
     st.subheader("Previous Reflections")
     for _, reflection in refl.iterrows():
         # Note: Using 'category' since 'event_name' doesn't exist in the reflections table
-        with st.expander(f"{reflection['category']} - {reflection['created_at'][:10]}"):
+            with st.expander(f"{reflection['category']} - {reflection['created_at'][:10]}"):
             st.write(reflection['content'])
             # Add any additional reflection details here
 
